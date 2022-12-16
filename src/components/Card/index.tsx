cf338a7788ffeaa4519ace0d4d37026bb5ef2713
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-12-16 11:03:02 -0300

    align text of Card to center

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,4 @@
-import { Card as ChakraCard, CardBody, CardHeader, CardFooter, Heading } from '@chakra-ui/react';
+import { Card as ChakraCard, CardBody, CardHeader, CardFooter, Heading, Text } from '@chakra-ui/react';
 
 interface InfoCardProps {
   title?: string | React.ReactNode;
@@ -8,11 +8,11 @@ interface InfoCardProps {
 
 const Card: React.FC<InfoCardProps> = ({ title, content, footer }) => {
   return (
-    <ChakraCard bgColor='var(--primary-dark)' >
+    <ChakraCard bgColor='var(--primary-dark)' textAlign='center' >
       <CardHeader p={3}>
         <Heading size='md' color='var(--font-dark)'>{title}</Heading>
       </CardHeader>
-      <CardBody p={3} color='var(--font-dark)'>{content}</CardBody>
+      <CardBody p={3} color='var(--font-dark)'><Text>{content}</Text></CardBody>
       {footer && (
         <CardFooter p={3}>{footer}</CardFooter>
       )}
