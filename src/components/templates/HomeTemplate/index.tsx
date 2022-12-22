bf0891fc1ace1ce8bf8faf92b4ab694522481ca8
Tony A. Gomes <-g>
2022-12-22 12:09:09 -0300

    add dividers to homepage

--- src/components/templates/HomeTemplate/index.tsx
+++ src/components/templates/HomeTemplate/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Divider } from "@chakra-ui/react";
 import { AiFillLinkedin, AiOutlineFacebook, AiOutlineInstagram } from "react-icons/ai";
 import { useMediaQuery } from "../../../hooks/useMediaQuery";
 import AnimatedLogo from "../../AnimatedLogo";
@@ -9,32 +10,37 @@ const HomeTemplate: React.FC = () => {
   const isMobile = useMediaQuery('mobile');
 
   return (
-    <Container
-      overflowX='hidden'
+    <Box
       display='flex'
       flexDir='column'
       justifyContent='space-between'
-      gap='6rem'
+      gap='4rem'
       pt='7rem'
     >
-     <AnimatedLogo isStatic={isMobile} /> 
-      <QuemSomosTemplate
-        resumo='Somos uma empresa inovadora, focada em projetar e desenvolver as melhores soluções de software para seus clientes. Trabalhamos com desenvolvimento de sistemas: web, móvel e desktop. Nossos especialistas em Inteligência Artificial, Visão Computacional, Segurança de Sistemas e Teste de Software contribuem para a excelência dos serviços que disponibilizamos neste segmento.'
-        missao='Oferecer serviços e soluções customizados, de alta qualidade e excelência, concretizando o retorno do investimento de nossos clientes.'
-        visao='Ser uma empresa sólida, lucrativa e inovadora, que proporcione qualidade de vida aos colaboradores e promova a transformação da sociedade a partir das novas tecnologias.'
-        valores='Nossos valores são os princípios norteados sobre os quais a NewGo foi fundada e como nos esforçamos para conduzir nossos negócios diariamente. Nossos principais valores são: TRANSPARÊNCIA, RESPEITO, DESAFIOS e RESPONSABILIDADE.'
-      />
-      <ContatosTemplate
-        email='contato@gmail.com'
-        telefone='+55 11 4752-2376'
-        whatsapp='+55 11 96314-4712'
-        redesSociais={[
-          { link: 'https://facebook.com', icon: <AiOutlineFacebook size={30} />},
-          { link: 'https://instagram.com', icon: <AiOutlineInstagram size={30} />},
-          { link: 'https://linkedin.com', icon: <AiFillLinkedin size={30} />}
-        ]}
-      />
-    </Container>
+      <Container overflowX='hidden'>
+        <AnimatedLogo isStatic={isMobile} />
+      </Container>
+      <Container display='flex' flexDir='column' gap='6rem' overflowX={isMobile ? 'hidden' : 'unset'}>
+        <Divider />
+        <QuemSomosTemplate
+          resumo='Somos uma empresa inovadora, focada em projetar e desenvolver as melhores soluções de software para seus clientes. Trabalhamos com desenvolvimento de sistemas: web, móvel e desktop. Nossos especialistas em Inteligência Artificial, Visão Computacional, Segurança de Sistemas e Teste de Software contribuem para a excelência dos serviços que disponibilizamos neste segmento.'
+          missao='Oferecer serviços e soluções customizados, de alta qualidade e excelência, concretizando o retorno do investimento de nossos clientes.'
+          visao='Ser uma empresa sólida, lucrativa e inovadora, que proporcione qualidade de vida aos colaboradores e promova a transformação da sociedade a partir das novas tecnologias.'
+          valores='Nossos valores são os princípios norteados sobre os quais a NewGo foi fundada e como nos esforçamos para conduzir nossos negócios diariamente. Nossos principais valores são: TRANSPARÊNCIA, RESPEITO, DESAFIOS e RESPONSABILIDADE.'
+        />
+        <Divider />
+        <ContatosTemplate
+          email='contato@gmail.com'
+          telefone='+55 11 4752-2376'
+          whatsapp='+55 11 96314-4712'
+          redesSociais={[
+            { link: 'https://facebook.com', icon: <AiOutlineFacebook size={30} />},
+            { link: 'https://instagram.com', icon: <AiOutlineInstagram size={30} />},
+            { link: 'https://linkedin.com', icon: <AiFillLinkedin size={30} />}
+          ]}
+        />
+      </Container>
+    </Box>
   );
 }
 
