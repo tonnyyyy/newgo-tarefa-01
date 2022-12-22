3a003449f127bdcfec3909d3ffa5a701cd3f8386
Tony A. Gomes <tony.gomes@newgo.com.br>
2022-12-22 02:04:49 -0300

    roboto font for all chakra Boxes

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
+import { HeaderHeightProvider } from './contexts/headerHeightContext';
+
+const theme = extendTheme({
+  components: {
+    Box: {
+      baseStyle: {
+        fontFamily: 'Roboto, sans-serif',
+      }
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <RouterProvider router={router} />
+    <ChakraProvider theme={theme}>
+      <HeaderHeightProvider>
+        <RouterProvider router={router} />
+      </HeaderHeightProvider>
     </ChakraProvider>
   </React.StrictMode>
 )
